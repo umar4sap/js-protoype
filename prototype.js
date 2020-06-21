68218e67e377c073155aeac07fe57aa35ebb0b86
Mohammad Umar <umarsoft.java@gmail.com>
2020-06-21 23:57:20 +0400

    added sample code

---
+++ prototype.js
@@ -0,0 +1,73 @@
+
+/**
+ * Author : Umar
+ */
+
+ 
+//Example of JavaScript Object Constructors & Prototyping in java script
+
+
+
+function Kid(age,name,city,msg){
+    this.age=age,
+    this.name=name,
+    this.city=city
+    this.showConsole=function(test,cb){
+        return test+1
+       
+    }
+}
+
+var kidData=new Kid(2,"musawwir","Dubai","enjoy life");
+kidData.showConsole("enjoy life");
+
+kidData.showconsole(2,function(data){console.log("gotdata",data)})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
